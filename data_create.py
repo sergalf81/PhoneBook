438b75e52be95d3161e7c4b21615ff1d3ece4296
Sergey A <sergalf81@mail.ru>
2024-01-30 19:48:28 +0300

    Добавлен файл data_create.py

---
+++ data_create.py
@@ -0,0 +1,18 @@
+def name_data():
+    name = input('Введите имя: ')
+    return name
+
+
+def surname_data():
+    surname = input('Введите фамилию: ')
+    return surname
+
+
+def phone_data():
+    phone = input('Введите номер телефона: ')
+    return phone
+
+
+def address_data():
+    address = input('Введите адрес: ')
+    return address
